c3f1f144d58bdf0d115b2ff9e5b787f35543cb6d
Om Patel <omipatel7113@gmail.com>
2025-08-15 12:57:30 +0530

    remove unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ import {
   Star,
   AlignRight,
   File,
-  Plus,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
